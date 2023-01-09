00fb94ec9909103f13a6a03d66385879b5ac1323
joysharon <joysharon@tilicho.in>
2023-01-09 18:28:11 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./cake.less";
+
+export default function App() {
+  return (
+    <div>
+      <div class="cake">
+        <div class="velas">
+          <div class="fuego"></div>
+          <div class="fuego"></div>
+          <div class="fuego"></div>
+          <div class="fuego"></div>
+          <div class="fuego"></div>
+        </div>
+        <div class="cobertura"></div>
+        <div class="bizcocho"></div>
+      </div>
+    </div>
+  );
+}
